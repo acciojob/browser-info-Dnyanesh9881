3735f2e1acf5c974a48d2856a940f4cdf79f9b99
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 02:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If requi
 // let division=document.createElement("div");
 let body=document.getElementById("browser-info");
-body.innerText="You are using "+navigator.userAgent+navigator.appVersion+navigator.appName;
+body.innerText="You are using "+navigator.userAgent+ " version " +navigator.appVersion;
 //   division.setAttribute(id, "browser-info");
 // body.appendChild(division);
