01a66821fb9a29438f98aa692002499292cc1342
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 01:45:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+//your JS code here. If requi
+let division=document.createElement("div");
+   division.textContent="you are using "+navigator.uerAgent+navigator.appName+navigator.appVersion;
+  division.setAttribute(id, "browser-info");
+document.body.appendChild(division);
